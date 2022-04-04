2cd8dcc329322409e10023e27be296ecc98def9c
Chris Torrence <chris_torrence@comcast.net>
2022-04-04 17:33:21 -0600

    Change timeout variables to make it easier to understand; add user-defined button function arrays

--- src/joystick.ts
+++ src/joystick.ts
@@ -1,7 +1,9 @@
 import { bank0 } from "./motherboard"
+import { addToBuffer } from "./keyboard"
 
-let paddle0 = 0
-let paddle1 = 0
+const maxTimeoutCycles = Math.trunc(0.0028*1.020484e6)
+let paddle0timeout = maxTimeoutCycles / 2
+let paddle1timeout = maxTimeoutCycles / 2
 let countStart = 0
 let leftAppleDown = false
 let rightAppleDown = false
@@ -22,6 +24,10 @@ export const pressAppleKey = (isDown: boolean, left: boolean) => {
   setButtonState()
 }
 
+const keyPress = (key: number) => {
+  bank0[0xC000] = key | 0b10000000
+}
+
 export const resetJoystick = (cycleCount: number) => {
   bank0[0xC064] = 0x80
   bank0[0xC065] = 0x80
@@ -31,34 +37,94 @@ export const resetJoystick = (cycleCount: number) => {
 }
 
 export const checkJoystickValues = (cycleCount: number) => {
-  const diff = (cycleCount - countStart)/11
-  bank0[0xC064] = (diff < paddle0) ? 0x80 : 0
-  bank0[0xC065] = (diff < paddle1) ? 0x80 : 0
+  const diff = cycleCount - countStart
+  bank0[0xC064] = (diff < paddle0timeout) ? 0x80 : 0
+  bank0[0xC065] = (diff < paddle1timeout) ? 0x80 : 0
 }
 
+const wolf = [
+  () => {leftButtonDown = true},
+  () => {rightButtonDown = true},
+  () => {keyPress('U'.charCodeAt(0))},
+  () => {keyPress('T'.charCodeAt(0))},
+  () => {leftButtonDown = true},
+  () => {rightButtonDown = true},
+  () => {keyPress(' '.charCodeAt(0))},
+  () => {keyPress(13)},
+  () => {leftButtonDown = true},
+  () => {rightButtonDown = true},
+  () => {leftButtonDown = true},
+  () => {rightButtonDown = true},
+  () => {paddle1timeout = 0},
+  () => {paddle1timeout = maxTimeoutCycles},
+  () => {paddle0timeout = 0},
+  () => {paddle0timeout = maxTimeoutCycles},
+]
+
+// AZTEC Controls
+// A, D: move left or right (while using weapon); face left or right (otherwise)
+// W: go to walk mode
+// R: go to run mode
+// C: go to climb mode (ascends steps or mounds)
+// J: jump
+// S: stop walking, running, or climbing
+// G: crawl once
+// P: set explosive (must be crawling)
+// O: open box or clear trash mound
+// L: look in box
+// T: take object from ground or box
+// Z: inventory
+// F: draw machete if you have one, gun otherwise, does nothin if unarmed
+// S: turn around while using weapon
+// L: lunge while using machete
+// M: stab at ground while using machete
+// G: switch from machete to gun
+// <spacebar>: fire gun
+let moving = false
+const aztec = [
+  () => {addToBuffer('J')},  // 0 A
+  () => {rightButtonDown = true},  // 1 B
+  () => {addToBuffer('U')},  // 2 X
+  () => {addToBuffer('T')},  // 3 Y
+  () => {leftButtonDown = true},  // 4 LB
+  () => {rightButtonDown = true},  // 5 RB
+  () => {addToBuffer('O'); addToBuffer('L'); addToBuffer('T')},  // 6 LT
+  () => {addToBuffer(' ')},  // 7 RT
+  () => {leftButtonDown = true},  // 8 Select?
+  () => {rightButtonDown = true},  // 9 Start?
+  () => {leftButtonDown = true},  // 10 Left thumb
+  () => {rightButtonDown = true},  // 11 Right thumb
+  () => {addToBuffer('R')},  // 12 D-pad U
+  () => {addToBuffer('W')},  // 13 D-pad D
+  () => {
+    addToBuffer(moving ? 'S' : 'A')
+    moving = !moving
+    if (moving) {addToBuffer('W')}},  // 14 D-pad L
+  () => {
+    addToBuffer(moving ? 'S' : 'D')
+    moving = !moving
+    if (moving) {addToBuffer('W')}},  // 15 D-pad R
+]
+
+let funcs = wolf
+funcs = aztec
+
 export const handleGamePad = (gamePad: Gamepad | null) => {
-    // window.addEventListener("gamepadconnected", function(e) {
-    //   console.log("Gamepad connected at index %d: %s. %d buttons, %d axes.",
-    //   e.gamepad.index, e.gamepad.id,
-    //   e.gamepad.buttons.length, e.gamepad.axes.length);
-    // })
   if (gamePad) {
-    const clip = 0.85
     let xstick = (gamePad.axes[0] !== 0) ? gamePad.axes[0] : gamePad.axes[2]
     let ystick = (gamePad.axes[1] !== 0) ? gamePad.axes[1] : gamePad.axes[3]
+    const dist = Math.sqrt(xstick * xstick + ystick * ystick)
+    const clip = 0.95 * ((xstick === 0 && ystick === 0) ? 1 :
+      Math.max(Math.abs(xstick), Math.abs(ystick)) / dist)
     xstick = Math.min(Math.max(-clip, xstick), clip)
     ystick = Math.min(Math.max(-clip, ystick), clip)
-    paddle0 = Math.trunc(255*(xstick + clip)/(2*clip))
-    paddle1 = Math.trunc(255*(ystick + clip)/(2*clip))
+    paddle0timeout = Math.trunc(maxTimeoutCycles*(xstick + clip)/(2*clip))
+    paddle1timeout = Math.trunc(maxTimeoutCycles*(ystick + clip)/(2*clip))
     leftButtonDown = false
     rightButtonDown = false
     gamePad.buttons.forEach((button, i) => {
-      if (i <= 11 && button.pressed) {
-        if (i % 2 === 0) {
-          leftButtonDown = true
-        } else {
-          rightButtonDown = true
-        }
+      if (button.pressed) {
+        funcs[i]()
       }
     });
     setButtonState()
