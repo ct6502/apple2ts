1d05b5efc36e5818259b5ff7945fa3686dd43149
Chris Torrence <chris_torrence@comcast.net>
2022-04-10 10:52:37 -0600

    If we have a 'new' audio signal, start it right away

--- public/worklet/oscillator.js
+++ public/worklet/oscillator.js
@@ -1,5 +1,5 @@
 class Oscillator extends AudioWorkletProcessor {
-  tick = new Array(100000);
+  tick = new Array(128000);
   index = 0;
   newIndex = 0;
   prevCycle = 0;
@@ -9,7 +9,15 @@ class Oscillator extends AudioWorkletProcessor {
     super();
     this.port.onmessage = (event) => {
       let delta = Math.round((event.data - this.prevCycle)*this.sampling*4)/4
-      this.newIndex = (this.newIndex + delta) % this.tick.length
+      // If we got a large audio delta, assume this is the start of a new sound
+      // and shift our starting index to just a few cycles downstream of the
+      // current (44100Hz) index. That way the sound starts immediately instead
+      // of looping around thru the entire tick array.
+      if (delta > 1024) {
+        this.newIndex = (this.index + 1024) % this.tick.length
+      } else {
+        this.newIndex = (this.newIndex + delta) % this.tick.length
+      }
       this.tick[Math.round(this.newIndex)] = 1
       this.prevCycle = event.data
     }
