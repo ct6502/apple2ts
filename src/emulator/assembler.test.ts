7dfc4e641771806f4373cdb20fcecaede2a5e2e5
Chris Torrence <5461379+ct6502@users.noreply.github.com>
2023-05-19 22:13:57 -0600

    Refactor assembler; add more unit tests

--- src/emulator/assembler.test.ts
+++ src/emulator/assembler.test.ts
@@ -1,66 +1,47 @@
-import { pcodes } from "./instructions";
-import { MODE } from "./utility";
 import { parseAssembly } from "./assembler";
 
+const addr = 0x300
+
 test('parseAssembly address modes', () => {
-  let a = parseAssembly(0x2000, [" BRK"])
-  expect(a).toEqual([0x00])
-  expect(pcodes[a[0]].mode).toEqual(MODE.IMPLIED)
+  expect(parseAssembly(addr, [" BRK"])).toEqual([0x00])
+  expect(parseAssembly(addr, [" LDA #65"])).toEqual([0xA9, 0x41])
+  expect(parseAssembly(addr, [" LDA #$C0"])).toEqual([0xA9, 0xC0])
+  expect(parseAssembly(addr, [" LDA $C0"])).toEqual([0xA5, 0xC0])
+  expect(parseAssembly(addr, [" LDA $C0,X"])).toEqual([0xB5, 0xC0])
+  expect(parseAssembly(addr, [" LDX $C0,Y"])).toEqual([0xB6, 0xC0])
+  expect(parseAssembly(addr, [" LDA $00C0"])).toEqual([0xAD, 0xC0, 0x00])
+  expect(parseAssembly(addr, [" LDA $1234"])).toEqual([0xAD, 0x34, 0x12])
+  expect(parseAssembly(addr, [" LDA $1234,X"])).toEqual([0xBD, 0x34, 0x12])
+  expect(parseAssembly(addr, [" LDA $1234,Y"])).toEqual([0xB9, 0x34, 0x12])
+  expect(parseAssembly(addr, [" LDA ($C0,X)"])).toEqual([0xA1, 0xC0])
+  expect(parseAssembly(addr, [" LDA ($C0),Y"])).toEqual([0xB1, 0xC0])
+  expect(parseAssembly(addr, [" JMP ($1234)"])).toEqual([0x6C, 0x34, 0x12])
+  expect(parseAssembly(addr, [" LDA ($C0)"])).toEqual([0xB2, 0xC0])
+  expect(parseAssembly(addr, [" JMP ($1234,X)"])).toEqual([0x7C, 0x34, 0x12])
+})
 
-  a = parseAssembly(0x2000, [" LDA #65"])
-  expect(a).toEqual([0xA9, 0x41])
-  expect(pcodes[a[0]].mode).toEqual(MODE.IMM)
+test('parseAssembly with labels', () => {
+  expect(parseAssembly(addr, ["ABC EQU $FA", " LDA ABC"])).toEqual([0xA5, 0xFA])
+  expect(parseAssembly(addr, ["ABC EQU $1234", " LDA ABC"])).toEqual([0xAD, 0x34, 0x12])
+})
+  
+test('parseAssembly with labels and math', () => {
+  expect(parseAssembly(addr, ["ABC EQU $FA", " LDA ABC+10"])).toEqual([0xAD, 0x04, 0x01])
+  expect(parseAssembly(addr, ["ABC EQU $FA", " LDA ABC+#10"])).toEqual([0xAD, 0x04, 0x01])
+  expect(parseAssembly(addr, ["ABC EQU $1234", " LDA ABC+3"])).toEqual([0xAD, 0x37, 0x12])
+  expect(parseAssembly(addr, ["ABC EQU $1234", " LDA ABC-76"])).toEqual([0xAD, 0xE8, 0x11])
+})
 
-  a = parseAssembly(0x2000, [" LDA #$C0"])
-  expect(a).toEqual([0xA9, 0xC0])
-  expect(pcodes[a[0]].mode).toEqual(MODE.IMM)
+test('parseAssembly with labels and promote/demote zp and abs', () => {
+  expect(parseAssembly(addr, ["ABC EQU $FA", " LDA ABC+$10"])).toEqual([0xAD, 0x0A, 0x01])
+  expect(parseAssembly(addr, ["ABC EQU $1234", " LDA ABC-$1230"])).toEqual([0xA5, 0x04])
+})
 
-  a = parseAssembly(0x2000, [" LDA $C0"])
-  expect(a).toEqual([0xA5, 0xC0])
-  expect(pcodes[a[0]].mode).toEqual(MODE.ZP_REL)
+test('parseAssembly with labels and wraparound', () => {
+  expect(parseAssembly(addr, ["ABC EQU $FFFE", " LDA ABC+$FF"])).toEqual([0xA5, 0xFD])
+  expect(parseAssembly(addr, ["ABC EQU $1234", " LDA ABC-$1236"])).toEqual([0xAD, 0xFE, 0xFF])
+})
 
-  a = parseAssembly(0x2000, [" LDA $C0,X"])
-  expect(a).toEqual([0xB5, 0xC0])
-  expect(pcodes[a[0]].mode).toEqual(MODE.ZP_X)
-
-  a = parseAssembly(0x2000, [" LDX $C0,Y"])
-  expect(a).toEqual([0xB6, 0xC0])
-  expect(pcodes[a[0]].mode).toEqual(MODE.ZP_Y)
-
-  a = parseAssembly(0x2000, [" LDA $00C0"])
-  expect(a).toEqual([0xAD, 0xC0, 0x00])
-  expect(pcodes[a[0]].mode).toEqual(MODE.ABS)
-
-  a = parseAssembly(0x2000, [" LDA $1234"])
-  expect(a).toEqual([0xAD, 0x34, 0x12])
-  expect(pcodes[a[0]].mode).toEqual(MODE.ABS)
-
-  a = parseAssembly(0x2000, [" LDA $1234,X"])
-  expect(a).toEqual([0xBD, 0x34, 0x12])
-  expect(pcodes[a[0]].mode).toEqual(MODE.ABS_X)
-
-  a = parseAssembly(0x2000, [" LDA $1234,Y"])
-  expect(a).toEqual([0xB9, 0x34, 0x12])
-  expect(pcodes[a[0]].mode).toEqual(MODE.ABS_Y)
-
-  a = parseAssembly(0x2000, [" LDA ($C0,X)"])
-  expect(a).toEqual([0xA1, 0xC0])
-  expect(pcodes[a[0]].mode).toEqual(MODE.IND_X)
-
-  a = parseAssembly(0x2000, [" LDA ($C0),Y"])
-  expect(a).toEqual([0xB1, 0xC0])
-  expect(pcodes[a[0]].mode).toEqual(MODE.IND_Y)
-
-  a = parseAssembly(0x2000, [" JMP ($1234)"])
-  expect(a).toEqual([0x6C, 0x34, 0x12])
-  expect(pcodes[a[0]].mode).toEqual(MODE.IND)
-
-  a = parseAssembly(0x2000, [" LDA ($C0)"])
-  expect(a).toEqual([0xB2, 0xC0])
-  expect(pcodes[a[0]].mode).toEqual(MODE.IND)
-
-  a = parseAssembly(0x2000, [" JMP ($1234,X)"])
-  expect(a).toEqual([0x7C, 0x34, 0x12])
-  expect(pcodes[a[0]].mode).toEqual(MODE.IND_X)
-
+test('parseAssembly with labels immediate mode', () => {
+  expect(parseAssembly(addr, ["ABC EQU $FA", " LDA #ABC"])).toEqual([0xA9, 0xFA])
 })
--- src/emulator/assembler.ts
+++ src/emulator/assembler.ts
@@ -3,7 +3,19 @@ import { toHex, isRelativeInstr, MODE } from "./utility";
 
 const doOutput = false
 
-const parseOperand = (operand: string): [MODE, number] => {
+type CodeLine = {
+  label: string,
+  instr: string,
+  operand: string,
+}
+
+type LabelOperand = {
+  label: string,
+  operation: string,
+  value: number
+}
+
+const parseNumberOptionalAddressMode = (operand: string): [MODE, number] => {
   let mode: MODE = MODE.IMPLIED;
   let value = -1
 
@@ -40,131 +52,148 @@ const parseOperand = (operand: string): [MODE, number] => {
 
 let labels: { [key: string]: number } = {};
 
-const getInstructionModeValue =
+const splitOperand = (operand: string) => {
+  const s = operand.split(/([+-])/)
+  const codeLine: LabelOperand = {
+    label: s[0] ? s[0] : '',
+    operation: s[1] ? s[1] : '',
+    value: s[2] ? parseInt(s[2].replace('#','').replace('$','0x')) : 0
+  }
+  return codeLine
+}
+
+const getOperandModeValue =
   (pc: number, instr: string, operand: string, pass: 1 | 2): [MODE, number] => {
     let mode = MODE.IMPLIED
     let value = -1
-    const opParts = operand.match(
-      /(?<num>[#$0-9()]*)(?<label>[A-Z]*)(?<labelSym>[+-]*)(?<labelOperand>[0-9]*)/)?.groups
-    if (opParts) {
-      if (opParts.num) {
-        [mode, value] = parseOperand(operand)
-      } else if (opParts.label) {
-        if (!(opParts.label in labels)) {
-          if (pass === 2) {
-            console.error("Missing label: " + opParts.label)
-            return [mode, value]
-          }
-        } else {
-          value = labels[opParts.label]
+    if (operand.match(/^[#]?[$0-9()]+/)) {
+      return parseNumberOptionalAddressMode(operand)
+    }
+    const labelOperand = splitOperand(operand)
+    if (labelOperand.label) {
+      // See if we have an immediate value, like #CONST
+      const isImmediate = labelOperand.label.startsWith('#')
+      if (isImmediate) {
+        labelOperand.label = labelOperand.label.substring(1)
+      }
+      if (labelOperand.label in labels) {
+        value = labels[labelOperand.label]
+      } else if (pass === 2) {
+          throw new Error("Missing label: " + labelOperand.label);
+      }
+      if (labelOperand.operation && labelOperand.value) {
+        switch (labelOperand.operation) {
+          case '+': value += labelOperand.value
+            break;
+          case '-': value -= labelOperand.value
+            break;
+          default:
+            throw new Error("Unknown operation in operand: " + operand);
         }
-        if (isRelativeInstr(instr)) {
-          mode = MODE.ZP_REL
-          value = (value - pc + 254) % 256
+        value = (value % 65536 + 65536) % 65536
+      }
+      if (isRelativeInstr(instr)) {
+        mode = MODE.ZP_REL
+        value = (value - pc + 254)
+      } else {
+        if (isImmediate) {
+          mode = MODE.IMM
         } else {
-          mode = MODE.ABS
-        }
-        if (opParts.labelSym && opParts.labelOperand) {
-          if (opParts.labelSym === '+') {
-            value += parseInt(opParts.labelOperand)
-          } else if (opParts.labelSym === '-') {
-            value -= parseInt(opParts.labelOperand)
-          }
+          mode = (value <= 255) ? MODE.ZP_REL : MODE.ABS
         }
       }
     }
     return [mode, value]
 }
 
+const splitLine = (line: string, prevLabel: string) => {
+  line = line.replace(/\s+/g, ' ')
+  const s = line.split(' ')
+  const codeLine: CodeLine = {
+    label: s[0] ? s[0] : prevLabel,
+    instr: s[1] ? s[1] : '',
+    operand: s[2] ? s[2] : ''
+  }
+  return codeLine
+}
+
+const handleLabel = (parts: CodeLine, pc: number) => {
+  if (parts.label in labels) {
+    throw new Error("Redefined label: " + parts.label)
+  }
+  if (parts.instr === 'EQU') {
+    const [mode, value] = parseNumberOptionalAddressMode(parts.operand)
+    if (mode !== MODE.ABS && mode !== MODE.ZP_REL) {
+      throw new Error("Illegal EQU value: " + parts.operand)
+    }
+    labels[parts.label] = value
+  } else {
+    labels[parts.label] = pc
+  }
+}
+
+const getHexCodesForInstruction = (match: number, value: number) => {
+  let newInstructions: Array<number> = [];
+  const pcode = pcodes[match]
+  newInstructions.push(match);
+  if (value >= 0) {
+    newInstructions.push(value % 256)
+    if (pcode.PC === 3) {
+      newInstructions.push(Math.trunc(value / 256))
+    }
+  }
+  return newInstructions
+}
+
 const parseOnce = (start: number, code: Array<string>, pass: 1 | 2): Array<number> => {
   let pc = start
   let instructions: Array<number> = [];
+  let prevLabel = ''
   code.forEach(line => {
-    let newInstructions: Array<number> = [];
-    let hexValue = ''
-
     line = (line.split(';'))[0].trimEnd().toUpperCase()
-
+    if (!line) return
     let output = (line + '                   ').slice(0, 30) + toHex(pc, 4) + "- "
 
-    const parts = line.match(/(?<label>[A-Z]*) *(?<instr>[A-Z]*) *(?<operand>.*)/)?.groups
-    if (line && parts) {
+    const codeLine = splitLine(line, prevLabel)
+    prevLabel = ''
 
-      // Remove all whitespace
-      parts.operand = parts.operand.replace(/\s/g, '')
-      if (parts.operand && !parts.instr) {
-        console.error("Missing instruction: " + line)
-        instructions = []
-        return
-      }
-
-      if (parts.instr === 'ORG') {
-        if (doOutput && pass === 2) console.log(output)
-        return
-      }
-
-      if (pass === 1 && parts.label) {
-        if (parts.label in labels) {
-          console.error("Redefined label: " + parts.label)
-          instructions = []
-          return
-        }
-        if (parts.instr === 'EQU') {
-          const [mode, value] = parseOperand(parts.operand)
-          if (mode !== MODE.ABS && mode !== MODE.ZP_REL) {
-            console.error("Illegal EQU value: " + parts.operand)
-            instructions = []
-            return
-          }
-          labels[parts.label] = value
-          if (doOutput) console.log(output)
-          return
-        } else {
-          labels[parts.label] = pc
-        }
-      }
-      if (parts.instr === 'EQU') {
-        return
-      }
-
-      const [mode, value] = getInstructionModeValue(pc, parts.instr, parts.operand, pass)
-
-      let match = -1
-      match = pcodes.findIndex(pc => pc && pc.name === parts.instr && pc.mode === mode)
-      if (match >= 0) {
-        const pcode = pcodes[match]
-        newInstructions.push(match);
-        output += toHex(match)
-        if (value >= 0) {
-          if (pcode.mode !== mode) {
-            console.error("Mismatch between instruction and mode: " + line)
-            instructions = []
-            return
-          }
-          newInstructions.push(value % 256)
-          output += ' ' + toHex(newInstructions[1])
-          if (pcode.PC === 3) {
-            newInstructions.push(Math.trunc(value / 256))
-            output += ' ' + toHex(newInstructions[2])
-            hexValue = toHex(value, 4)
-          } else {
-            output += "   "
-            hexValue = toHex(value)
-          }
-        } else {
-          output += "      "
-        }
-        pc += pcode.PC
-      } else {
-        console.error("Unknown instruction: " + line)
-        instructions = []
-        return
-      }
-
-      output += '  ' + parts.instr + (hexValue !== '' ? " $" + hexValue : '')
-      if (doOutput && pass === 2) console.log(output)
-      instructions.push(...newInstructions)
+    // Just a label by itself, just tack onto the beginning of next line.
+    if (!codeLine.instr) {
+      prevLabel = codeLine.label
+      return
     }
+
+    if (codeLine.instr === 'ORG') {
+      if (doOutput && pass === 2) console.log(output)
+      return
+    }
+
+    if (pass === 1 && codeLine.label) {
+      handleLabel(codeLine, pc)
+    }
+
+    if (codeLine.instr === 'EQU') {
+      return
+    }
+
+    const [mode, value] = getOperandModeValue(pc, codeLine.instr, codeLine.operand, pass)
+
+    if (isRelativeInstr(codeLine.instr) && (value < 0 || value > 255)) {
+      throw new Error("Branch instruction out of range: " + line);
+    }
+
+    const match = pcodes.findIndex(pc => pc && pc.name === codeLine.instr && pc.mode === mode)
+    if (match < 0) {
+      throw new Error("Unknown instruction: " + line);
+    }
+    const newInstructions = getHexCodesForInstruction(match, value)
+    pc += pcodes[match].PC
+
+    if (doOutput && pass === 2) {
+      newInstructions.forEach(i => {output += ` ${toHex(i)}`});
+      console.log(output)
+    }
+    instructions.push(...newInstructions)
   });
 
   return instructions
@@ -172,8 +201,13 @@ const parseOnce = (start: number, code: Array<string>, pass: 1 | 2): Array<numbe
 
 export const parseAssembly = (start: number, code: Array<string>): Array<number> => {
   labels = {}
-  parseOnce(start, code, 1)
-  const instructions = parseOnce(start, code, 2)
-  return instructions
+  try {
+    parseOnce(start, code, 1)
+    const instructions = parseOnce(start, code, 2)
+    return instructions
+  } catch (error) {
+    console.error(error)
+    return []
+  }
 }
 
