eb3f68d8f6cc9930232f983bc33e2d82b8e019a4
Chris Torrence <5461379+ct6502@users.noreply.github.com>
2023-08-04 23:00:44 -0600

    Add comments to explain audio; add hack for very fast pwm music

--- public/worklet/oscillator.js
+++ public/worklet/oscillator.js
@@ -1,7 +1,26 @@
+// Algorithm from Kent Dickey (KFest discord, 7/27/2023):
+// Keep track of the speaker clicks, let's say you do it in cycles.
+// Let's assume you need to create 44.1KHz samples. You remember the cycle
+// time of the last sample you've sent to be played (start at 0). Now time
+// has passed and you need to generate N more samples. Convert the time of
+// the speaker clicks in samples, using 1.020484M cycles per 44100 samples
+// (so if the speaker clicked at cycle offset 201, that would be sample
+// 8.68. You need to remember if the speaker was high or low, so let's
+// assume it was high. Let's assume you have 5 speaker clicks:
+// sample 8.1, 8.68, 8.8, and 30.5. Then the audio samples will be:
+// [0..7]=HIGH; [8]=(.1+(8.8-8.68))HIGH, [9...29]=LOW, [30]=0.5HIGH, [31...N]=HIGH.
+// You calculate the contribution of the time the speaker is high to be the
+// multiplier of the high time. Any samples where the speaker didn't toggle
+// just repeats the speaker low/high position. 
+
 class Oscillator extends AudioWorkletProcessor {
-  tick = new Array(16384 * 10).fill(0);
+  tick = new Array(16384 * 4).fill(0);
+  len = this.tick.length
   playbackIndex = 0;
-  currIndex = 1024;
+  // This minimum offset was picked so a BASIC program that just prints BELL
+  // characters won't trigger the "new sound" delta down below.
+  offset = 1524
+  currIndex = this.offset;
   currSample = 0;
   sampling = 44100/1.020484e6;
   value = 1
@@ -9,6 +28,10 @@ class Oscillator extends AudioWorkletProcessor {
   constructor() {
     super();
     this.port.onmessage = (event) => {
+      // if (this.doprint < 1000) {
+      //   console.log(`onmessage ${this.currIndex}`)
+      //   this.doprint++
+      // }
       let newSample = event.data * this.sampling
       let delta = newSample - this.currSample
       this.currSample = newSample
@@ -16,30 +39,36 @@ class Oscillator extends AudioWorkletProcessor {
       // and shift our starting index to just a few cycles downstream of the
       // current (44100Hz) index. That way the sound starts immediately instead
       // of looping around thru the entire tick array.
-      if (delta > 1024) {
-        this.currIndex = (this.playbackIndex + 1024) % this.tick.length
+      if (delta > this.offset) {
+        this.currIndex = (this.playbackIndex + this.offset) % this.len
       } else {
         let newIndex = this.currIndex + delta
-        let i0 = Math.floor(this.currIndex) % this.tick.length
-        let i1 = Math.floor(newIndex) % this.tick.length
+        let i0 = Math.floor(this.currIndex) % this.len
+        let i1 = Math.floor(newIndex) % this.len
+        // Current and new cycle counts fall within the same 1/44100 sample.
         if (i1 === i0) {
+          // Add the fractional contribution of just the delta.
           this.tick[i0] = this.tick[i0] + delta * this.value
         } else {
+          // Add the fractional contribution of the first index.
           this.tick[i0] = this.tick[i0] + (1.0 - this.currIndex % 1) * this.value
-//          if (this.doprint < 100) console.log(`${this.currIndex} ${i0} ${this.tick[i0]}`)
-          i0 = (i0 + 1) % this.tick.length
+          i0 = (i0 + 1) % this.len
+          // All subsequent indices have a contribution of +/-1
           while (i0 !== i1) {
             this.tick[i0] = this.value
-            i0 = (i0 + 1) % this.tick.length
+            i0 = (i0 + 1) % this.len
+            if (i0 === this.playbackIndex) {
+              // Hack: If we wrapped around and caught up to our current
+              // playback position, just back up the playback. Produces a
+              // slight playback hiccup but avoids catastrophic noise.
+              this.playbackIndex = (this.playbackIndex + this.len - this.offset) % this.len
+            }
           }
+          // Last index includes the fractional contribution of the last index.
           this.tick[i1] = (newIndex % 1) * this.value
         }
-        this.currIndex = newIndex % this.tick.length
-        // if (this.doprint < 100) {
-        //   console.log(`${this.currIndex} ${i0} ${this.tick[i0]}`)
-        //   console.log(`------------------------------`)
-        // }
-        // this.doprint++
+        this.currIndex = newIndex % this.len
+        // Flip the speaker in/out and vice versa
         this.value = -this.value
       }
     }
@@ -51,7 +80,7 @@ class Oscillator extends AudioWorkletProcessor {
     for (let i = 0; i < channel.length; i++) {
       channel[i] = 0.2 * this.tick[this.playbackIndex]
       this.tick[this.playbackIndex] = 0
-      this.playbackIndex = (this.playbackIndex + 1) % this.tick.length;
+      this.playbackIndex = (this.playbackIndex + 1) % this.len;
     }
     return true;
   }
