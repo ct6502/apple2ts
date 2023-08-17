0356b1fa9c226c90b0c0d50c23c5176e8d440317
code-bythepound <codebythepound@gmail.com>
2023-08-16 23:38:59 -0600

    Add methods to control slot IO

--- src/emulator/memory.ts
+++ src/emulator/memory.ts
@@ -125,6 +125,31 @@ export const updateAddressTables = () => {
 }
 
 export const specialJumpTable = new Map<number, () => void>();
+const slotIOJumpTable = new Array<(a:number, value: number, isRead:boolean) => number>(8)
+
+const checkSlotIO = (addr : number): any => {
+  if (addr < 0xC090 || addr > 0xC0FF)
+    return null;
+
+  addr >>= 4;
+  addr &= 7;
+
+  let f = slotIOJumpTable[addr]
+  if (f === undefined)
+    return null
+  else
+    return f
+}
+
+/**
+ * Add peripheral card IO callback.
+ *
+ * @param slot - The slot number 1-7.
+ * @param fn - A function to jump to when IO of this slot is accessed
+ */
+export const setSlotIODriver = (slot: number, fn: (a:number, v: number, isRead:boolean) => number) => {
+  slotIOJumpTable[slot] = fn;
+}
 
 /**
  * Add peripheral card ROM.
@@ -192,6 +217,9 @@ const memGetSoftSwitch = (addr: number, code=0): number => {
     return ((cycleCount % 17030) > 12480) ? 0x0D : 0x8D
   }
   checkSoftSwitches(addr, false, cycleCount)
+  const func = checkSlotIO(addr)
+  if (func)
+    return func(addr, 0, true)
   if (addr >= SWITCHES.DRVSM0.offAddr && addr <= SWITCHES.DRVWRITE.onAddr) {
     return handleDriveSoftSwitches(addr, -1)
   }
@@ -217,9 +245,15 @@ const memSetSoftSwitch = (addr: number, value: number) => {
   if (addr >= SWITCHES.DRVSM0.offAddr && addr <= SWITCHES.DRVWRITE.onAddr) {
     handleDriveSoftSwitches(addr, value)
   } else {
-    checkSoftSwitches(addr, true, cycleCount)
-    if (addr <= 0xC00F || addr >= 0xC050) {
-      updateAddressTables()
+    const func = checkSlotIO(addr)
+    if (func) {
+      func(addr, value, false)
+      // do i need to call updateAddressTables() here?
+    } else {
+      checkSoftSwitches(addr, true, cycleCount)
+      if (addr <= 0xC00F || addr >= 0xC050) {
+        updateAddressTables()
+      }
     }
   }
 }
