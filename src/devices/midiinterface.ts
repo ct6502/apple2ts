1831e31511803ca466dd11d559dd19df2dfc3adb
code-bythepound <codebythepound@gmail.com>
2024-01-22 22:26:38 -0700

    added new parsed format for patches and instruments

--- src/devices/midiinterface.ts
+++ src/devices/midiinterface.ts
@@ -115,6 +115,7 @@ const initDevices = () => {
 // execute on load
 connect();
 
+let u5midi = true;
 let once = true;
 let buffer: number[] = [];
 
@@ -142,6 +143,14 @@ let buffer: number[] = [];
 //
 // U-110                GM
 // ----------------     --------------------
+// I-24:A.GUITAR 1      26 - Acoustic Guitar (steel) 
+// I-45:FINGERED 1      34 - Electric Bass (fingered)
+// I-55:CHOIR 1         53 - Choir Aahs
+// I-57:CHOIR 3         54 - Voice Oohs
+// I-59:STRINGS 1       49 - String Ensemble 1
+// I-61:STRINGS 3       50 - String Ensemble 2
+// I-76:SOFT TP 1       57 - Trumpet
+// I-95:FLUTE 1         74 - Flute
 // 1-01:HARPSI 1        07 - Harpsichord
 // 1-06:HARPSI 6        07 - Harpsichord
 // 1-09:POSITIVE 3      19 - Rock Organ
@@ -159,282 +168,209 @@ let buffer: number[] = [];
 // 6-17:CLARINET 6      72 - Clarinet
 // 6-25:FR.HORN 3       61 - French Horn
 // 6-35:TIMPANI 2       117 - Taiko Drum
-// I-24:A.GUITAR 1      26 - Acoustic Guitar (steel) 
-// I-45:FINGERED 1      34 - Electric Bass (fingered)
-// I-55:CHOIR 1         53 - Choir Aahs
-// I-57:CHOIR 3         54 - Voice Oohs
-// I-59:STRINGS 1       49 - String Ensemble 1
-// I-61:STRINGS 3       50 - String Ensemble 2
-// I-76:SOFT TP 1       57 - Trumpet
-// I-95:FLUTE 1         74 - Flute
 
-// Parts below are from U110 mapping
-// Part  Instrument  Key Range  Level
+// NOTE: All numbers here are 1-based, but in MIDI file and Protocol they
+//       are zero based.
+const InstU110ToGM = [
+  [0,24, 26],
+  [0,45, 34],
+  [0,55, 53],
+  [0,57, 54],
+  [0,59, 49],
+  [0,61, 50],
+  [0,76, 57],
+  [0,95, 74],
+  [1,1, 7],
+  [1,6, 7],
+  [1,9, 19],
+  [1,15, 20],
+  [1,20, 21],
+  [3,22,16],
+  [5,1, 41],
+  [5,2, 42],
+  [5,4, 43],
+  [5,8, 43],
+  [5,10, 47],
+  [6,2, 69],
+  [6,12, 72],
+  [6,13, 72],
+  [6,17, 72],
+  [6,25, 61],
+  [6,35, 115]
+];
+
+// The output assigns the instrument stereo positioning
+//
+// Left       Right
+//        2
+// 0  4   3   5  1
+const OutputToBalance = [
+  0,
+  31,
+  63,
+  63,
+  94,
+  127
+];
+
+// Parts below are from U110 mapping taken from the .MID produced by 
+//
+const UG = {
+  ReceiveChannel: 0,
+  ToneMedia: 1,
+  ToneNumber: 2,
+  KeyRangeLow: 3,
+  KeyRangeHigh: 4,
+  Level: 5,
+  ProgramOutput: 6
+};
+//
 const u110ToGM = [
 // Default patch 0
 [
-[0, 0x00, 0x7f, 35],
-[0, 0x00, 0x7f, 35],
-[0, 0x00, 0x7f, 35],
-[0, 0x00, 0x7f, 35],
-[0, 0x00, 0x7f, 35],
-[0, 0x00, 0x7f, 35],
+[0, 0, 0, 0, 0x7f, 35, 3],
+[0, 0, 0, 0, 0x7f, 35, 3],
+[0, 0, 0, 0, 0x7f, 35, 3],
+[0, 0, 0, 0, 0x7f, 35, 3],
+[0, 0, 0, 0, 0x7f, 35, 3],
+[0, 0, 0, 0, 0x7f, 35, 3],
 ],
-/*
-Patch1: UltimaThm 
-0: 0:75 30->5b 28
-1: 0:58 00->7f 20
-2: 6:24 30->3c 28
-3: 0:75 2f->47 23
-4: 0:75 48->54 25
-*/
+//Patch: UltimaThm - OMode: 0:11  1:8  2:4  3:4  4:4
 [
-[56, 0x30, 0x5b, 28],
-[48, 0x00, 0x7f, 20],
-[60, 0x30, 0x3c, 20],
-[56, 0x2f, 0x47, 23],
-[56, 0x48, 0x54, 25],
-[ 0, 0x00, 0x00,  0],
+[0, 0, 75, 48, 91, 28, 1],
+[0, 0, 58, 0, 127, 20, 0],
+[0, 6, 24, 48, 60, 28, 2],
+[0, 0, 75, 47, 71, 23, 3],
+[0, 0, 75, 72, 84, 25, 4],
+[5, 0, 1, 0, 127, 127, 6],
 ],
-
-/*
-Patch2: BritncLnds
-0: 5:9 00->4a 55
-1: 0:58 00->7f 17
-2: 6:34 00->2b 127
-3: 5:9 43->7f 65
-4: 1:8 3c->7f 23
-*/
+//Patch: BritncLnds - OMode: 0-1:S16  2:3  3:4  4:4  5:4
 [
-[46, 0x00, 0x4a, 55],
-[48, 0x00, 0x7f, 17],
-[116, 0x00, 0x2b, 127],
-[46, 0x43, 0x7f, 65],
-[18, 0x3c, 0x7f, 23],
-[ 0, 0x00, 0x00,  0],
+[0, 5, 9, 0, 74, 55, 4],
+[0, 0, 58, 0, 127, 17, 1],
+[0, 6, 34, 0, 43, 127, 1],
+[0, 5, 9, 67, 127, 65, 5],
+[0, 1, 8, 60, 127, 23, 0],
+[5, 0, 11, 0, 127, 127, 6],
 ],
-
-/*
-Patch3: CJsHrnpipe
-0: 5:0 00->58 55
-1: 0:58 00->3a 23
-2: 6:12 41->7f 60
-3: 6:12 01->40 50
-*/
+//Patch: CJsHrnpipe - OMode: 0-1:S16  2:3  3:4  4:4  5:4
 [
-[40, 0x00, 0x58, 55],
-[48, 0x00, 0x3a, 23],
-[71, 0x41, 0x7f, 60],
-[71, 0x01, 0x40, 50],
-[ 0, 0x00, 0x00,  0],
-[ 0, 0x00, 0x00,  0],
+[0, 5, 0, 0, 88, 55, 0],
+[0, 0, 58, 0, 58, 23, 4],
+[0, 6, 12, 65, 127, 60, 5],
+[0, 6, 12, 1, 64, 50, 3],
+[4, 0, 2, 0, 126, 127, 6],
+[5, 0, 2, 0, 127, 127, 6],
 ],
-
-/*
-Patch4: Eng.&Melee
-0: 0:75 30->54 7
-1: 0:58 00->53 20
-2: 6:24 30->3c 28
-3: 5:0 56->63 100
-4: 0:75 48->54 16
-5: 6:34 00->30 44
-*/
+//Patch: Eng.&Melee - OMode: 0:11  1:8  2:4  3:4  4:4
 [
-[56, 0x30, 0x54, 7],
-[48, 0x00, 0x53, 20],
-[60, 0x30, 0x3c, 28],
-[40, 0x56, 0x63, 100],
-[56, 0x48, 0x54, 16],
-[116, 0x00, 0x30, 44],
+[0, 0, 75, 48, 84, 7, 1],
+[0, 0, 58, 0, 83, 20, 0],
+[0, 6, 24, 48, 60, 28, 2],
+[0, 5, 0, 86, 99, 100, 3],
+[0, 0, 75, 72, 84, 16, 4],
+[0, 6, 34, 0, 48, 44, 3],
 ],
-
-/*
-Patch5: Stones
-0: 3:21 00->4d 127
-1: 0:54 40->7f 8
-*/
+//Patch: Stones - OMode: 0-1:S31
 [
-[15, 0x00, 0x4d, 127],
-[52, 0x40, 0x7f, 8],
-[ 0, 0x00, 0x00,  0],
-[ 0, 0x00, 0x00,  0],
-[ 0, 0x00, 0x00,  0],
-[ 0, 0x00, 0x00,  0],
+[0, 3, 21, 0, 77, 127, 0],
+[0, 0, 54, 64, 127, 8, 1],
+[2, 0, 19, 0, 127, 127, 6],
+[3, 0, 19, 0, 127, 127, 6],
+[4, 0, 19, 0, 127, 127, 6],
+[5, 0, 19, 0, 127, 127, 6],
 ],
-
-/*
-Patch6: GreysnTale
-0: 0:23 00->7f 80
-1: 6:16 3c->7f 25
-2: 0:44 00->2f 26
-3: 0:94 34->48 15
-4: 6:34 00->33 127
-*/
+//Patch: GreysnTale - OMode: 0-1:S16  2:3  3:4  4:4  5:4
 [
-[25, 0x00, 0x7f, 80],
-[71, 0x3c, 0x7f, 25],
-[33, 0x00, 0x2f, 26],
-[73, 0x34, 0x48, 15],
-[116, 0x00, 0x33, 127],
-[ 0, 0x00, 0x00,  0],
+[0, 0, 23, 0, 127, 80, 1],
+[0, 6, 16, 60, 127, 25, 5],
+[0, 0, 44, 0, 47, 26, 4],
+[0, 0, 94, 52, 72, 15, 3],
+[0, 6, 34, 0, 51, 127, 6],
+[5, 0, 19, 0, 127, 127, 6],
 ],
-
-/*
-Patch7: Fanfare4V
-0: 0:75 28->5b 28
-1: 0:58 00->7f 20
-2: 6:24 30->3c 28
-3: 0:75 2f->47 23
-4: 0:75 48->54 25
-5: 6:34 00->30 54
-*/
+//Patch: Fanfare - OMode: 0:11  1:8  2:4  3:4  4:4
 [
-[56, 0x28, 0x5b, 28],
-[48, 0x00, 0x7f, 20],
-[60, 0x30, 0x3c, 28],
-[56, 0x2f, 0x47, 23],
-[56, 0x48, 0x54, 25],
-[116, 0x00, 0x30, 54],
+[0, 0, 75, 40, 91, 28, 1],
+[0, 0, 58, 0, 127, 20, 0],
+[0, 6, 24, 48, 60, 28, 2],
+[0, 0, 75, 47, 71, 23, 3],
+[0, 0, 75, 72, 84, 25, 4],
+[0, 6, 34, 0, 48, 54, 3],
 ],
-
-/*
-Patch8: MsngMnarc
-0: 5:3 30->5b 25
-1: 0:58 00->7f 24
-2: 6:24 30->3c 28
-3: 5:0 2f->47 23
-4: 6:34 00->2e 55
-*/
-
+//Patch: MsngMnarc - OMode: 0:11  1:8  2:4  3:4  4:4
 [
-[42, 0x30, 0x5b, 25],
-[48, 0x00, 0x7f, 24],
-[60, 0x30, 0x3c, 28],
-[40, 0x2f, 0x47, 23],
-[116, 0x00, 0x2e, 55],
-[ 0, 0x00, 0x00,  0],
+[0, 5, 3, 48, 91, 25, 0],
+[0, 0, 58, 0, 127, 24, 1],
+[0, 6, 24, 48, 60, 28, 2],
+[0, 5, 0, 47, 71, 23, 4],
+[0, 6, 34, 0, 46, 55, 3],
+[5, 0, 1, 0, 127, 127, 6],
 ],
-
-/*
-Patch9: Tarantella
-0: 5:7 00->7f 32
-1: 6:16 3c->7f 32
-2: 0:44 00->2f 8
-3: 6:1 34->48 20
-4: 1:5 00->2f 43
-*/
-
+//Patch: Tarantella - OMode: 0-1:S16  2:3  3:4  4:4  5:4
 [
-[42, 0x00, 0x7f, 32],
-[71, 0x3c, 0x7f, 32],
-[33, 0x00, 0x2f,  8],
-[68, 0x34, 0x48, 20],
-[ 6, 0x00, 0x2f, 43],
-[ 0, 0x00, 0x00,  0],
+[0, 5, 7, 0, 127, 32, 1],
+[0, 6, 16, 60, 127, 32, 5],
+[0, 0, 44, 0, 47, 8, 4],
+[0, 6, 1, 52, 72, 20, 3],
+[0, 1, 5, 0, 47, 43, 2],
+[5, 0, 19, 0, 127, 127, 6],
 ],
-
-/*
-Patch10: HallOfDoom
-0: 1:0 00->7f 61
-1: 0:58 00->7f 13
-2: 6:34 00->2b 120
-*/
-
+//Patch: HallOfDoom - OMode: 0-1:S31
 [
-[ 6, 0x00, 0x7f, 61],
-[48, 0x00, 0x7f, 13],
-[116, 0x00, 0x2b, 120],
-[ 0, 0x00, 0x00,  0],
-[ 0, 0x00, 0x00,  0],
-[ 0, 0x00, 0x00,  0],
+[0, 1, 0, 0, 127, 61, 0],
+[0, 0, 58, 0, 127, 13, 1],
+[0, 6, 34, 0, 43, 120, 1],
+[3, 0, 9, 0, 127, 127, 6],
+[4, 0, 9, 0, 127, 127, 6],
+[5, 0, 11, 0, 127, 127, 6],
 ],
-
-/*
-Patch11: WrldsBelow
-0: 5:1 00->7f 90
-1: 0:60 00->7f 55
-2: 6:34 00->2d 100
-3: 6:11 44->51 32
-4: 1:0 00->51 17
-*/
-
+//Patch: WrldsBelow - OMode: 0-1:S16  2:3  3:4  4:4  5:4
 [
-[41, 0x00, 0x7f, 90],
-[49, 0x00, 0x7f, 55],
-[116, 0x00, 0x2d, 100],
-[71, 0x44, 0x51, 32],
-[ 6, 0x00, 0x51, 17],
-[ 0, 0x00, 0x00,  0],
+[0, 5, 1, 0, 127, 90, 4],
+[0, 0, 60, 0, 127, 55, 0],
+[0, 6, 34, 0, 45, 100, 2],
+[0, 6, 11, 68, 81, 32, 4],
+[0, 1, 0, 0, 81, 17, 5],
+[5, 0, 11, 0, 127, 127, 6],
 ],
-
-/*
-Patch12: LordBlkthn
-0: 1:14 00->7f 25
-1: 1:19 00->4e 15
-2: 0:54 42->52 85
-*/
+//Patch: LordBlkthn - OMode: 0-1:S16  2:7  3:8
 [
-[19, 0x00, 0x7f, 25],
-[20, 0x00, 0x4e, 15],
-[52, 0x42, 0x52, 85],
-[ 0, 0x00, 0x00,  0],
-[ 0, 0x00, 0x00,  0],
-[ 0, 0x00, 0x00,  0],
+[0, 1, 14, 0, 127, 25, 0],
+[0, 1, 19, 0, 78, 15, 3],
+[0, 0, 54, 66, 82, 85, 2],
+[0, 0, 11, 0, 127, 127, 6],
+[0, 0, 11, 0, 127, 127, 6],
+[0, 0, 17, 0, 127, 127, 6],
 ],
-
-/*
-Patch13: DreamOfNan
-0: 5:9 00->4d 56
-1: 0:56 3f->7f 4
-2: 0:58 00->6c 6
-*/
-
+//Patch: DreamOfNan - OMode: 0-1:S31
 [
-[46, 0x00, 0x4d, 56],
-[53, 0x3f, 0x7f, 4],
-[48, 0x00, 0x6c, 6],
-[ 0, 0x00, 0x00, 0],
-[ 0, 0x00, 0x00, 0],
-[ 0, 0x00, 0x00, 0],
+[0, 5, 9, 0, 77, 56, 0],
+[0, 0, 56, 63, 127, 4, 1],
+[0, 0, 58, 0, 108, 6, 6],
+[3, 0, 12, 0, 127, 127, 6],
+[4, 0, 19, 0, 127, 127, 6],
+[5, 0, 19, 0, 127, 127, 6],
 ],
-
-/*
-Patch14: JoyousReu
-0: 0:75 30->5b 28
-1: 0:58 00->7f 20
-2: 6:24 30->3c 28
-3: 0:75 2f->47 23
-4: 6:34 00->30 55
-*/
-
+//Patch: JoyousReu - OMode: 0:11  1:8  2:4  3:4  4:4
 [
-[56, 0x30, 0x5b, 28],
-[48, 0x00, 0x7f, 20],
-[60, 0x30, 0x3c, 28],
-[56, 0x2f, 0x47, 23],
-[116, 0x00, 0x30, 55],
-[ 0, 0x00, 0x00, 0],
+[0, 0, 75, 48, 91, 28, 1],
+[0, 0, 58, 0, 127, 20, 0],
+[0, 6, 24, 48, 60, 28, 2],
+[0, 0, 75, 47, 71, 23, 4],
+[0, 6, 34, 0, 48, 55, 3],
+[5, 0, 1, 0, 127, 127, 6],
 ],
-
-/*
-Patch15: RuleBritn 
-0: 0:94 18->5b 22
-1: 0:58 00->7f 15
-2: 6:24 30->3c 28
-3: 0:75 2f->47 23
-4: 0:75 48->54 25
-*/
-
+//Patch: RuleBritn - OMode: 0:11  1:8  2:4  3:4  4:4
 [
-[73, 0x18, 0x5b, 22],
-[48, 0x00, 0x7f, 15],
-[60, 0x30, 0x3c, 28],
-[56, 0x2f, 0x47, 23],
-[56, 0x48, 0x54, 25],
-[ 0, 0x00, 0x00,  0],
+[0, 0, 94, 24, 91, 22, 1],
+[0, 0, 58, 0, 127, 15, 0],
+[0, 6, 24, 48, 60, 28, 2],
+[0, 0, 75, 47, 71, 23, 3],
+[0, 0, 75, 72, 84, 25, 4],
+[5, 0, 1, 0, 127, 127, 6],
 ]
 ];
-
 
 let gmNoteLookup = new Array(0);
 
@@ -444,22 +380,68 @@ const setupGMPatch = (which: number) => {
   gmNoteLookup = new Array(128);
   gmNoteLookup.fill(0);
 
+  if (which === 0)
+    return;
+
   const patch = u110ToGM[which];
   console.log("Patch: ", which, patch);
 
-  //[inst: 00, start: 0x00, end: 0x7f, volume: 35],
   for(let i=0;i<6;i++)
   {
-    if (patch[i][3] === 0)  // check volume
-      break;
+    // If program output is > 5, channel is unused
+    if (patch[i][UG.ProgramOutput] > 5)
+      continue;
+
+    // find GM instrument
+    let gmi = 128;
+    const TM = patch[i][UG.ToneMedia];
+    const TN = patch[i][UG.ToneNumber] + 1;
+    for(let z=0;z<InstU110ToGM.length;z++)
+    {
+      if (TM === InstU110ToGM[z][0] && TN === InstU110ToGM[z][1] )
+      {
+        gmi = InstU110ToGM[z][2] - 1;
+        break;
+      }
+    }
+
+    // XXX hack
+    //if (gmi === 116)
+    //  continue;
+
+    if (gmi === 128)
+    {
+      console.log("Instrument (" + TM + " " + TN + ") not found!");
+      gmi = 1; //default to piano?
+    }
+
+    console.log("Channel " + i + " inst: " + gmi + " Range: " + patch[i][UG.KeyRangeLow].toString(16) + "-" + patch[i][UG.KeyRangeHigh].toString(16));
 
     // send change program
-    device.send( [0xc0+i, patch[i][0]] );
+    let arr = [0xc0+i, gmi];
+    //console.log(arr);
+    device.send( arr );
     // send change volume
-    device.send( [0xb0+i, 0x07, patch[i][3]] );
+    let vol = patch[i][UG.Level] * 2;   // volumes seem way too quiet
+    if (vol > 127)
+      vol = 127;
+    arr = [0xb0+i, 0x07, vol];
+    //console.log(arr);
+    //device.send( arr );
+
+    const pan = OutputToBalance[patch[i][UG.ProgramOutput]];
+
+    // send change balance
+    //arr = [0xb0+i, 0x08, pan];
+    //console.log(arr);
+    //device.send( arr );
+    // send change pan
+    arr = [0xb0+i, 0x0a, pan];
+    //console.log(arr);
+    device.send( arr );
 
     // set notes used in this patch
-    for(let j=patch[i][1];j<patch[i][2];j++)
+    for(let j=patch[i][UG.KeyRangeLow];j<=patch[i][UG.KeyRangeHigh];j++)
       gmNoteLookup[j] |= (1<<i);
   }
 }
@@ -469,33 +451,52 @@ const parseMsg = (msg: number[]) => {
 
   let notes = 0;
   const no = msg[0];
-  // only check for channel change and note on/off here
-  switch(no)
+  if (u5midi)
   {
-    case 0xC0:
-      setupGMPatch(msg[1]);
-      return;
-    case 0x80:
-    case 0x90:
-      notes = gmNoteLookup[msg[1]];
-      break;
+    // only check for channel change and note on/off here
+    switch(no)
+    {
+      case 0xC0:
+        setupGMPatch(msg[1]);
+        return;
+      case 0x80:
+      case 0x90:
+        notes = gmNoteLookup[msg[1]];
+        break;
+    }
   }
 
+  let msgs: number[] = [];
   if (notes)
   {
+    //let nn = "012345";
+    //let nx = "";
     for(let i=0;i<6;i++)
     {
       if (notes & (1<<i))
       {
+        //nx = nx + ( no==0x80?"X":"O" );
         msg[0] = no+i;
-        device.send(msg);
+        msgs = msgs.concat(msg);
+        //device.send(msg);
+      }
+      else
+      {
+        //nx = nx + " ";
       }
     }
+
+    //console.log(nn);
+    //console.log(nx);
   }
   else
   {
-    device.send(msg);
+    msgs = msgs.concat(msg);
+    //console.log( no==0x80?"X":"O" );
   }
+
+  //console.log(msgs.map(function (x) {return x.toString(16);}).toString());
+  device.send(msgs);
 }
 
 export const receiveMidiData = (data: Uint8Array) => {
