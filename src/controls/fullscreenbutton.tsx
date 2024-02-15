f2c3c27f1806017de26fea8a947be6c40d44b82b
Chris Torrence <5461379+ct6502@users.noreply.github.com>
2024-02-14 21:32:00 -0700

    Fix full screen button

--- src/controls/fullscreenbutton.tsx
+++ src/controls/fullscreenbutton.tsx
@@ -3,10 +3,6 @@ import {
   faExpand,
 } from "@fortawesome/free-solid-svg-icons";
 
-type RequestFullScreen_Type = {
-  call: (context: HTMLCanvasElement) => void
-}
-
 const FullScreenButton = () => {
   const isTouchDevice = "ontouchstart" in document.documentElement
   return (
@@ -16,18 +12,10 @@ const FullScreenButton = () => {
         const canvas = document.getElementById('apple2canvas') as HTMLCanvasElement
         const context = canvas.getContext('2d')
         if (context) {
-          let requestFullScreen: RequestFullScreen_Type | null = null
-          if ('webkitRequestFullscreen' in context) {
-            requestFullScreen = context.webkitRequestFullscreen as RequestFullScreen_Type
-          } else if ('mozRequestFullScreen' in context) {
-            requestFullScreen = context.mozRequestFullScreen as RequestFullScreen_Type
-          } else if ('msRequestFullscreen' in context) {
-            requestFullScreen = context.msRequestFullscreen as RequestFullScreen_Type
-          } else if ('requestFullscreen' in context) {
-            requestFullScreen = context.requestFullscreen as RequestFullScreen_Type
-          }
-          if (requestFullScreen) {
-            requestFullScreen.call(canvas);
+          try {
+            canvas.requestFullscreen()
+          } catch (e) {
+            // do nothing
           }
         }
       }
