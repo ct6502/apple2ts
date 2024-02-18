5cc23c045ba3f4235aa9abfa30d827b374c6a3f6
Chris Torrence <5461379+ct6502@users.noreply.github.com>
2024-02-18 15:56:42 -0700

    Upgrade Total Replay to v5.1. Thanks 4am, qkumba, and everyone else involved

--- src/devices/diskimages.ts
+++ src/devices/diskimages.ts
@@ -9,7 +9,7 @@ export const diskImages: Array<diskImage> = [
   {file: 'One on One.po', url: 'https://archive.org/details/electronic-arts-one-on-one-a2-ph/'},
   {file: 'Pitch Dark.hdv', url: 'https://archive.org/details/PitchDark'},
   {file: 'ProDOS 2.4.2.po', url: 'https://prodos8.com/'},
-  {file: 'Total Replay v5.0.hdv', url: 'https://archive.org/details/TotalReplay'},
+  {file: 'Total Replay v5.1.hdv', url: 'https://archive.org/details/TotalReplay'},
   {file: 'Ultima IV.hdv', url: 'https://wiki.ultimacodex.com/wiki/Ultima_IV:_Quest_of_the_Avatar'},
   {file: 'Ultima V.hdv', url: 'https://wiki.ultimacodex.com/wiki/Ultima_V:_Warriors_of_Destiny'},
   {file: 'Wizard Replay v1.0.hdv', url: 'https://archive.org/details/WizardReplay'}
