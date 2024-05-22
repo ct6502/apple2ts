7721eaada526edbf6ee7ccfad0ac69c5ea784837
Chris Torrence <5461379+ct6502@users.noreply.github.com>
2024-05-22 10:15:15 -0600

    turn off debug flag :-)

--- src/emulator/utility/utility.ts
+++ src/emulator/utility/utility.ts
@@ -1,6 +1,6 @@
 import { KeyboardEvent } from "react"
 
-export const TEST_DEBUG = true
+export const TEST_DEBUG = false
 export const TEST_GRAPHICS = false
 export const MAX_SNAPSHOTS = 30
 export const FILE_SUFFIXES = ".a2ts,.hdv,.2mg,.dsk,.woz,.po,.do,.bin"
