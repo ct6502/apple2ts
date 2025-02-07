0881f540dada9726d6635adba435e63f1d0df930
Lawrence Sanchez <boredsenseless@hotmail.com>
2025-02-07 14:30:30 -0800

    Remove commented code

--- src/emulator/utility/onedriveclouddrive.ts
+++ src/emulator/utility/onedriveclouddrive.ts
@@ -71,7 +71,6 @@ export class OneDriveCloudDrive implements CloudDrive {
           this.lastSyncTime = Date.now()
           this.syncInterval = DEFAULT_SYNC_INTERVAL
 
-          // return await this.sync(blob)
           return true
       }
     }
