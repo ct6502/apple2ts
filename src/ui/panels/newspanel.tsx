aded7a49be09db561902bb0a9ff00ba93d3a983e
Lawrence Sanchez <boredsenseless@hotmail.com>
2025-03-16 22:55:19 -0700

    Added release date to new release tiles

--- src/ui/panels/newspanel.tsx
+++ src/ui/panels/newspanel.tsx
@@ -9,29 +9,34 @@ import { handleSetDiskFromURL } from "../devices/driveprops"
 // $TODO: Read this from a hosted JSON file
 interface NewsArticle {
   title: string,
+  releaseDate: string,
   imageUrl: string,
   diskUrl: string
 }
 const newsArticles: NewsArticle[] = [
   {
     title: "Glider for Apple II",
+    releaseDate: "3/16/2025",
     imageUrl: "https://www.colino.net/wordpress/wp-content/uploads/glider-splash.png",
     diskUrl: "https://colino.net/tmp/glider-en-beta-202503162.po"
   },
   {
+    title: "Million Perfect Tiles",
+    releaseDate: "12/30/2024",
+    imageUrl: "https://ia800300.us.archive.org/16/items/MillionPerfectTiles/00playable_screenshot.png",
+    diskUrl: "https://archive.org/download/MillionPerfectTiles/Million.Perfect.Tiles.v1.1.po"
+  },
+  {
     title: "Encounter Adventure",
+    releaseDate: "11/11/2024",
     imageUrl: "https://www.brutaldeluxe.fr/products/apple2/encounter/title.jpg",
     diskUrl: "https://www.brutaldeluxe.fr/products/apple2/encounter/encounteradventure.dsk"
   },
   {
     title: "Undead Demo",
+    releaseDate: "9/10/2024",
     imageUrl: "https://www.callapple.org/wp-content/uploads/2024/09/Undead_Demo.png",
     diskUrl: "https://www.callapple.org/wp-content/uploads/2024/09/UNDEAD_DEMO.po_.zip"
-  },
-  {
-    title: "Million Perfect Tiles",
-    imageUrl: "https://ia800300.us.archive.org/16/items/MillionPerfectTiles/00playable_screenshot.png",
-    diskUrl: "https://archive.org/download/MillionPerfectTiles/Million.Perfect.Tiles.v1.1.po"
   }
 ]
 
@@ -43,6 +48,12 @@ const NewsPanel = () => {
     handleSetDiskFromURL(newsArticles[articleIndex].diskUrl)
     setIsFlyoutOpen(false)
   }
+
+  const dateFormatter = new Intl.DateTimeFormat('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  });
 
   if (isMinimalTheme) {
     import("./newspanel.minimal.css")
@@ -58,7 +69,8 @@ const NewsPanel = () => {
       position="top-center">
       <div className="news-panel">
         {newsArticles.map((newsArticle, index) => (
-          <div key={`np-article-${index}`} className="np-article" >
+          <div key={`np-article-${index}`} className="np-article">
+            <div className="np-article-release-date">{dateFormatter.format(new Date(newsArticle.releaseDate))}</div>
             <img className="np-article-image" src={newsArticle.imageUrl} title={newsArticle.title} onClick={handleArticlelick(index)} />
           </div>
         ))}
