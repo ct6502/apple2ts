0e28198228d575056ef622360375df8cada5b05c
Lawrence Sanchez <boredsenseless@hotmail.com>
2025-03-17 17:17:00 -0700

    Better disk collection item sizing for different screen resolutions

--- src/ui/panels/diskcollectionpanel.tsx
+++ src/ui/panels/diskcollectionpanel.tsx
@@ -7,36 +7,36 @@ import { diskImages } from "../devices/diskimages"
 import { replaceSuffix } from "../../common/utility"
 
 // $TODO: Read this from a hosted JSON file
-// const diskCollectionItems: DiskCollectionItem[] = [
-// {
-//   title: "Glider for Apple II",
-//   releaseDate: "3/16/2025",
-//   imageUrl: "https://www.colino.net/wordpress/wp-content/uploads/glider-splash.png",
-//   diskUrl: "https://colino.net/tmp/glider-en-beta-202503162.po"
-// },
-// {
-//   title: "Million Perfect Tiles",
-//   releaseDate: "12/30/2024",
-//   imageUrl: "https://ia800300.us.archive.org/16/items/MillionPerfectTiles/00playable_screenshot.png",
-//   diskUrl: "https://archive.org/download/MillionPerfectTiles/Million.Perfect.Tiles.v1.1.po"
-// },
-// {
-//   title: "Encounter Adventure",
-//   releaseDate: "11/11/2024",
-//   imageUrl: "https://www.brutaldeluxe.fr/products/apple2/encounter/title.jpg",
-//   diskUrl: "https://www.brutaldeluxe.fr/products/apple2/encounter/encounteradventure.dsk"
-// },
-// {
-//   title: "Undead Demo",
-//   releaseDate: "9/10/2024",
-//   imageUrl: "https://www.callapple.org/wp-content/uploads/2024/09/Undead_Demo.png",
-//   diskUrl: "https://www.callapple.org/wp-content/uploads/2024/09/UNDEAD_DEMO.po_.zip"
-// }
-// ]
+const newReleases: DiskCollectionItem[] = [
+  {
+    title: "Glider for Apple II",
+    lastUpdated: new Date("3/16/2025"),
+    imageUrl: "https://www.colino.net/wordpress/wp-content/uploads/glider-splash.png",
+    diskUrl: "https://colino.net/tmp/glider-en-beta-202503162.po"
+  },
+  {
+    title: "Million Perfect Tiles",
+    lastUpdated: new Date("12/30/2024"),
+    imageUrl: "https://ia800300.us.archive.org/16/items/MillionPerfectTiles/00playable_screenshot.png",
+    diskUrl: "https://archive.org/download/MillionPerfectTiles/Million.Perfect.Tiles.v1.1.po"
+  },
+  {
+    title: "Encounter Adventure",
+    lastUpdated: new Date("11/11/2024"),
+    imageUrl: "https://www.brutaldeluxe.fr/products/apple2/encounter/title.jpg",
+    diskUrl: "https://www.brutaldeluxe.fr/products/apple2/encounter/encounteradventure.dsk"
+  },
+  {
+    title: "Undead Demo",
+    lastUpdated: new Date("9/10/2024"),
+    imageUrl: "https://www.callapple.org/wp-content/uploads/2024/09/Undead_Demo.png",
+    diskUrl: "https://www.callapple.org/wp-content/uploads/2024/09/UNDEAD_DEMO.po_.zip"
+  }
+]
 
 const dateFormatter = new Intl.DateTimeFormat('en-US', {
   year: 'numeric',
-  month: 'long',
+  month: 'numeric',
   day: 'numeric'
 });
 
@@ -44,6 +44,7 @@ const DiskCollectionPanel = (props: DisplayProps) => {
   const [isFlyoutOpen, setIsFlyoutOpen] = useState(false)
   const diskCollectionItems: DiskCollectionItem[] = []
 
+  // Load built-in disk images
   diskImages.forEach((diskImage) => {
     diskCollectionItems.push({
       title: diskImage.title,
@@ -51,6 +52,11 @@ const DiskCollectionPanel = (props: DisplayProps) => {
       detailsUrl: diskImage.url,
       diskImage: diskImage
     })
+  })
+
+  // Load new releases
+  newReleases.forEach((diskImage) => {
+    diskCollectionItems.push(diskImage)
   })
 
   const handleItemClick = (itemIndex: number) => () => {
@@ -74,13 +80,15 @@ const DiskCollectionPanel = (props: DisplayProps) => {
       title="disk collection"
       isOpen={() => { return isFlyoutOpen }}
       onClick={() => { setIsFlyoutOpen(!isFlyoutOpen) }}
-      width="max( 70vw, 200px )"
+      width="max( 75vw, 200px )"
       position="top-center">
       <div className="disk-collection-panel">
         {diskCollectionItems.map((diskCollectionItem, index) => (
           <div key={`dcp-item-${index}`} className="dcp-item" onClick={handleItemClick(index)}>
-            <div className="dcp-item-title">{diskCollectionItem.title}</div>
-            {diskCollectionItem.lastUpdated && <div className="dcp-item-release">dateFormatter(diskCollectionItem.lastUpdated)</div>}
+            <div className="dcp-item-title-box">
+              <div className="dcp-item-title">{diskCollectionItem.title}</div>
+            </div>
+            {diskCollectionItem.lastUpdated && <div className="dcp-item-updated">{dateFormatter.format(diskCollectionItem.lastUpdated)}</div>}
             <div className="dcp-item-image-box">
               <img className="dcp-item-image" src={diskCollectionItem.imageUrl} title={diskCollectionItem.title} />
             </div>
