28bbc02344c53663ac3e25f510817e4db0892e25
Lawrence Sanchez <boredsenseless@hotmail.com>
2025-03-17 18:46:53 -0700

    Added help icon support

--- src/ui/devices/diskimagedialog.tsx
+++ src/ui/devices/diskimagedialog.tsx
@@ -23,7 +23,6 @@ const DiskImageDialog = (props: DiskImageDialogProps) => {
         style={{ left: "5%", top: "2%" }}>
         <div style={{
           display: "inline",
-          // margin: "10px",
           gap: "10px",
           width: width,
           overflowX: "hidden",
