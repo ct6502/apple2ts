05e664c36b25afacfe3b928eb10ede0f27165152
Lawrence Sanchez <boredsenseless@hotmail.com>
2025-04-11 21:15:46 -0700

    Fix scanlines top alignment bug

--- src/ui/canvas.tsx
+++ src/ui/canvas.tsx
@@ -420,7 +420,7 @@ const Apple2Canvas = (props: DisplayProps) => {
     if (document.fullscreenElement !== myCanvas?.current?.parentElement) {
       if (getTheme() == UI_THEME.MINIMAL) {
         scanlinesLeft = (window.innerWidth - scanlinesWidth) / 2
-        scanlinesTop = ((window.innerHeight - scanlinesHeight) / 2) - 10
+        scanlinesTop = ((window.innerHeight - scanlinesHeight) / 2) - 32
 
         if (handleGetIsDebugging()) {
           const debugSection = document.getElementsByClassName("flyout-bottom-right")[0] as HTMLElement
