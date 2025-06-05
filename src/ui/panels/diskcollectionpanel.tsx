3ad710c9e3f441efb93b3f3846c42d8a0f2c990c
Lawrence Sanchez <boredsenseless@hotmail.com>
2025-06-04 17:45:25 -0700

    Improved theme handling for export button

--- src/ui/panels/diskcollectionpanel.tsx
+++ src/ui/panels/diskcollectionpanel.tsx
@@ -441,6 +441,8 @@ const DiskCollectionPanel = (props: DisplayProps) => {
           <div className="dcp-export-size">Estimated HDV size: <b><span className={`${estimateHdvSize() > maxHdvBytes ? "dcp-export-size-exceeded" : ""}`}>{formatBytes(estimateHdvSize())}</span> / {formatBytes(maxHdvBytes)}</b></div>
           <button className="dcp-export-button" disabled={isExportButtonDisabled()} onClick={handleExportClick}>Export</button>
         </div>}
+      {activeTab != TAB_INDEX_SELECT &&
+        <div className="dcp-export-row-empty" />}
       <PopupMenu
         key="drive-popup"
         location={drivePopupLocation}
