f812be578aef6897763750c88a95742681548557
Chris Torrence <5461379+ct6502@users.noreply.github.com>
2025-10-05 07:59:28 -0600

    Fix IA collections image links; remove preload of images

--- src/ui/devices/disk/internetarchivedialog.tsx
+++ src/ui/devices/disk/internetarchivedialog.tsx
@@ -36,22 +36,22 @@ const softwareCollections: SoftwareCollection[] = [
   {
     id: "softwarelibrary_apple",
     title: "The Software Library: Apple Computer",
-    imageUrl: "/collections/softwarelibrary_apple_itemimage.jpg"
+    imageUrl: "collections/softwarelibrary_apple_itemimage.jpg"
   },
   {
     id: "softwarelibrary_apple_games",
     title: "The Apple II Library: Games",
-    imageUrl: "/collections/softwarelibrary_apple_games_itemimage.jpg"
+    imageUrl: "collections/softwarelibrary_apple_games_itemimage.jpg"
   },
   {
     id: "softwarelibrary_apple_woz_educational",
     title: "Software Library: Apple Educational",
-    imageUrl: "/collections/softwarelibrary_apple_woz_educational_itemimage.jpg"
+    imageUrl: "collections/softwarelibrary_apple_woz_educational_itemimage.jpg"
   },
   {
     id: "apple_ii_library_4am",
     title: "Apple II Library: The 4am Collection",
-    imageUrl: "/collections/apple_ii_library_4am_itemimage.jpg"
+    imageUrl: "collections/apple_ii_library_4am_itemimage.jpg"
   }
 ]
 
