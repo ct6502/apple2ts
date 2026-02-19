2cd3d0829478ccf370f75c823af5f75c45680c04
Chris Torrence <5461379+ct6502@users.noreply.github.com>
2026-02-18 19:57:38 -0700

    When hovering over a softswitch in disassembly, return correct value

--- src/ui/panels/disassemblyview_singleline.tsx
+++ src/ui/panels/disassemblyview_singleline.tsx
@@ -19,6 +19,9 @@ const getShiftedMemoryValue = (addr: number) => {
     if (memory.length > 1) {
       const addressGetTable = handleGetAddressGetTable()
       const page = addr >>> 8
+      if (page === 0xC0) {
+        return memory[0x10000 + (addr & 255)]
+      }
       const shifted = addressGetTable[page]
       addr = shifted + (addr & 255)
       if (addr < memory.length) {
