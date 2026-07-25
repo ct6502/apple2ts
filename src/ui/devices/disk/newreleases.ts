9dc1ed10527713bf567eab61bd8ec44d9f56d299
anomixer <anomy@mail.com>
2026-07-26 04:00:01 +0800

    fix: Restore original remote disk URLs and OneDrive client settings in newreleases

--- src/ui/devices/disk/newreleases.ts
+++ src/ui/devices/disk/newreleases.ts
@@ -4,7 +4,7 @@ export const newReleases: DiskCollectionItem[] = [
     title: "Glider for Apple\u00A0II",
     lastUpdated: new Date("3/16/2025"),
     imageUrl: "https://www.colino.net/wordpress/wp-content/uploads/glider-splash.png",
-    diskUrl: "GliderII.po",
+    diskUrl: "https://colino.net/tmp/glider-en-beta-202503162.po",
     detailsUrl: "https://www.colino.net/wordpress/glider-for-apple-ii/",
     params: "color=green",
     fileSize: 143360
@@ -14,7 +14,7 @@ export const newReleases: DiskCollectionItem[] = [
     title: "Million Perfect Tiles",
     lastUpdated: new Date("12/30/2024"),
     imageUrl: "https://archive.org/download/MillionPerfectTiles/00playable_screenshot.png",
-    diskUrl: "MillionPerfectTiles.po",
+    diskUrl: "https://archive.org/download/MillionPerfectTiles/00playable.po",
     detailsUrl: "https://archive.org/details/MillionPerfectTiles",
     fileSize: 143360
   },
@@ -23,7 +23,7 @@ export const newReleases: DiskCollectionItem[] = [
     title: "Encounter Adventure",
     lastUpdated: new Date("11/11/2024"),
     imageUrl: "https://www.brutaldeluxe.fr/products/apple2/encounter/title.jpg",
-    diskUrl: "EncounterAdventure.dsk",
+    diskUrl: "https://github.com/antoinevignau/source/raw/refs/heads/main/encounter/encounteradventure.dsk",
     detailsUrl: "https://www.brutaldeluxe.fr/products/apple2/encounter/",
     fileSize: 143360
   },
@@ -32,7 +32,7 @@ export const newReleases: DiskCollectionItem[] = [
     title: "Undead Demo",
     lastUpdated: new Date("9/10/2024"),
     imageUrl: "https://www.callapple.org/wp-content/uploads/2024/09/Undead_Demo.png",
-    diskUrl: "UndeadDemo.po",
+    diskUrl: "https://www.callapple.org/wp-content/uploads/2024/09/UNDEAD_DEMO.po_.zip",
     detailsUrl: "https://www.kickstarter.com/projects/8-bit-shack/undead-a-new-apple-role-player-game?utm_source=a2central",
     fileSize: 819200
   },
@@ -41,7 +41,7 @@ export const newReleases: DiskCollectionItem[] = [
     title: "Kontrabant",
     lastUpdated: new Date("4/10/2025"),
     imageUrl: "https://www.brutaldeluxe.fr/products/apple2/kontrabant/Kontrabant%20Box.jpg",
-    diskUrl: "Kontrabant.dsk",
+    diskUrl: "https://github.com/antoinevignau/source/raw/refs/heads/main/kontrabant/kontra_en.dsk",
     detailsUrl: "https://www.brutaldeluxe.fr/products/apple2/kontrabant/",
     fileSize: 819200
   },
@@ -50,7 +50,7 @@ export const newReleases: DiskCollectionItem[] = [
     title: "Shufflepuck Cafe",
     lastUpdated: new Date("4/23/2025"),
     imageUrl: "https://www.colino.net/wordpress/wp-content/uploads/shufflepuck-cafe-bar.png",
-    diskUrl: "Shufflepuck.po",
+    diskUrl: "https://github.com/colinleroy/a2tools/releases/latest/download/shufflepuck.po",
     detailsUrl: "https://www.colino.net/wordpress/en/shufflepuck-cafe-for-apple-ii/",
     params: "color=green",
     fileSize: 143360
@@ -59,8 +59,8 @@ export const newReleases: DiskCollectionItem[] = [
     type: -1,
     title: "K2 Presentation",
     lastUpdated: new Date("5/2/2025"),
-    imageUrl: "https://raw.githubusercontent.com/rsheehan/K2/main/images/intro.png",
-    diskUrl: "K2.po",
+    imageUrl: "https://github.com/rsheehan/K2/raw/refs/heads/main/images/intro.png",
+    diskUrl: "https://github.com/rsheehan/K2/raw/refs/heads/main/K2.po",
     detailsUrl: "https://github.com/rsheehan/K2",
     params: "TEXT=%2BK2%20ABOUTK2",
     fileSize: 143360
@@ -69,8 +69,8 @@ export const newReleases: DiskCollectionItem[] = [
     type: -1,
     title: "A2DeskTop",
     lastUpdated: new Date("7/31/2025"),
-    imageUrl: "disks/a2desktop.jpg",
-    diskUrl: "A2DeskTop.2mg",
+    imageUrl: "https://github.com/ct6502/apple2ts/raw/refs/heads/main/public/disks/a2desktop.jpg",
+    diskUrl: "https://a2desktop.s3.amazonaws.com/A2DeskTop-1.5-alpha4-en_800k.2mg",
     detailsUrl: "https://github.com/a2stuff/a2d",
     params: "color=white&speed=fast&scanlines=off",
     fileSize: 819264,
@@ -85,4 +85,5 @@ export const newReleases: DiskCollectionItem[] = [
     detailsUrl: "https://www.the8bitguy.com/product/quarx-for-apple-iie-iic-iigs-and-laser-128/",
     fileSize: 143360,
   },
+
 ]
