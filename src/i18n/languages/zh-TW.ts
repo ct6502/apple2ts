37ca624ed548bfbd7073e075643acd90d65a3bd0
3AMCinnamonRoll <19525653+3amcinnamonroll@users.noreply.github.com>
2026-08-07 09:41:47 -0700

    fix(i18n): use fallback for disassembly readouts

--- src/i18n/languages/zh-TW.ts
+++ src/i18n/languages/zh-TW.ts
@@ -95,10 +95,10 @@ export const zhTW = {
         "keyboard": "Keyboard = \"{{character}}\"; Strobe is {{state}} (bit 7 = {{bit}})",
         "auxiliaryBankUnknown": "Select auxiliary bank using {{addressing}} addressing; start paddle timers",
         "auxiliaryBank": "Select auxiliary bank {{bank}} using {{addressing}} addressing; start paddle timers",
-        "preIndexedAddress": "Address: {{effectiveAddress}} ⇐ ({{base}} + {{index}} = {{pointer}})",
-        "indexedAddress": "Address: {{effectiveAddress}} = {{base}} + {{index}}",
-        "indirectAddress": "Address: {{effectiveAddress}}",
-        "value": "Value: {{value}}"
+        "preIndexedAddress": "({{base}} + {{index}} = {{pointer}}) => address = {{effectiveAddress}}",
+        "indexedAddress": "address {{base}} + {{index}} = {{effectiveAddress}}",
+        "indirectAddress": "address = {{effectiveAddress}}",
+        "value": "value = {{value}}"
       },
       "states": {
         "1": "1",
