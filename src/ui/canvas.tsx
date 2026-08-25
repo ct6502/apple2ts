037e02b3efa3567f66dbf7bf4f352da1f62ae362
3AMCinnamonRoll <19525653+3amcinnamonroll@users.noreply.github.com>
2026-08-25 12:37:10 -0700

    fix(ui): prevent canvas focus from scrolling (#359)

--- src/ui/canvas.tsx
+++ src/ui/canvas.tsx
@@ -611,7 +611,7 @@ const Apple2Canvas = (props: DisplayProps) => {
 
   const setFocus = () => {
     if (mainCanvas) {
-      (mainCanvas as HTMLCanvasElement).focus()
+      (mainCanvas as HTMLCanvasElement).focus({ preventScroll: true })
     }
   }
 
