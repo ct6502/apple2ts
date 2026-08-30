2177917cd169f9a8b410fdcdf99b6c281b41393d
Lawrence Sanchez <lawrence@huang-sanchez.com>
2026-08-30 11:12:01 -0700

    - Restored previous 'drive.file' scope to avoid Google Drive security warning (#411)
    - Updated Google Drive control panel to use UI picker to browse for disks

--- src/ui/devices/disk/googledrive_retro.ts
+++ src/ui/devices/disk/googledrive_retro.ts
@@ -1,19 +1,12 @@
 import type { RetroControlMetadata } from "../../retro/retromenucontext"
-import { createRetroCloudDriveControl } from "./clouddrive_retro"
 import { loadDiskFromCloudDrive } from "./diskdrive"
 import { GoogleDrive } from "./googledrive"
 
-export const createRetroGoogleDriveControl = (driveIndex: number): RetroControlMetadata => {
-  const googleDrive = new GoogleDrive()
-  const control = createRetroCloudDriveControl(driveIndex, {
+export const createRetroGoogleDriveControl = (driveIndex: number): RetroControlMetadata => ({
     id: `diskDrives.${driveIndex}.load.googleDrive`,
-    displayName: "Google Drive",
-    loadLabelKey: "disk.loadDiskFromGoogleDrive",
-    hasAuthToken: () => googleDrive.hasAuthToken(),
-    signIn: () => googleDrive.signIn(),
-    listFolder: folderId => googleDrive.listFolder(folderId),
-    loadFile: (item, targetDriveIndex) =>
-      loadDiskFromCloudDrive(new GoogleDrive(item), targetDriveIndex),
+    label: context => context.t("disk.loadDiskFromGoogleDrive"),
+    actionLabel: context => context.t("retroControl.load"),
+    action: async context => {
+      if (await loadDiskFromCloudDrive(new GoogleDrive(), driveIndex)) context.close()
+    },
   })
-  return control
-}
