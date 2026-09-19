6198548b9d66bdb8af0df87ad16c4a55229dff9b
Chris Torrence <5461379+ct6502@users.noreply.github.com>
2026-09-19 17:02:31 -0600

    Clean up touch joystick, add button numbers

--- src/ui/controls/touchjoystick.tsx
+++ src/ui/controls/touchjoystick.tsx
@@ -12,15 +12,19 @@ let oldAxis0 = 0
 let oldAxis1 = 0
 
 export const TouchJoystick = () => {
+  const [stickDown, setStickDown] = useState<[number, number]>([-1, -1])
+  const [stickOffset, setStickOffset] = useState<{ x: number, y: number }>({ x: 0, y: 0 })
+  const [buttonStates, setButtonStates] = useState<[boolean, boolean]>([false, false])
 
-  const [eventCounter, setEventCounter] = useState<number>(0)
+  const isTouchDevice = "ontouchstart" in document.documentElement
+  const defaultOpacity = isTouchDevice ? "0.25" : "0.5"
 
   const doSetCustomGamepad = (buttons: boolean[] | null, axes: number[] | null) => {
-    if (!getUIStateBoolean("touchJoystick")) {
+    if (getUIStateBoolean("touchJoystick")) {
+      setCustomGamepad(buttons, axes)
+    } else {
       clearCustomGamepad()
-      return
     }
-    setCustomGamepad(buttons, axes)
   }
 
   const scaleJoystick = (value: number, yMin: number, yMax: number, xMin: number, xMax: number) => {
@@ -57,37 +61,38 @@ export const TouchJoystick = () => {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
-  const toggleButton = (buttonNumber: number, enabled: boolean) => {
-    const button = document.getElementById(`tj-button${buttonNumber}`) as HTMLElement
-    // Make the button look "pressed"
-    if (button) {
-      button.style.opacity = `${enabled ? 0.95 : 0.25}`
-    }
-  }
-
-  const handlePointerEnter = (event: React.PointerEvent) => {
+  const handlePointerDown = (event: React.PointerEvent) => {
+    event.currentTarget.setPointerCapture(event.pointerId)
+    setStickDown([event.clientX, event.clientY])
+    setStickOffset({ x: 0, y: 0 })
     handlePointerMove(event)
   }
 
   const handlePointerMove = (event: React.PointerEvent) => {
+    if (!event.currentTarget.hasPointerCapture(event.pointerId)) {
+      return
+    }
     event.preventDefault()
+    const joystick = document.getElementById("touchjoystick-stick") as HTMLElement
+    joystick.style.opacity = "0.95"
 
-    setEventCounter(eventCounter + 1)
-
-    const currentTarget = event.currentTarget as HTMLElement
-    const rect = currentTarget.getBoundingClientRect()
-
-    const stickLeft = 25
-    const stickRight = (rect.right - 20)
-    const stickTop = rect.top + rect.width * 0.12
-    const stickBottom = rect.top + rect.height - rect.width * 0.12
-    
-    const offsetX = event.clientX - currentTarget.clientLeft
-    const offsetY = event.clientY
+    let stickX = stickDown[0]
+    let stickY = stickDown[1]
+    if (stickX < 0) {
+      stickX = event.clientX
+      stickY = event.clientY
+      setStickDown([event.clientX, event.clientY])
+    }
+    const offsetX = event.clientX - stickX
+    const offsetY = event.clientY - stickY
+    const maxStickDistance = 70
+    const clampedOffsetX = Math.max(-maxStickDistance, Math.min(maxStickDistance, offsetX))
+    const clampedOffsetY = Math.max(-maxStickDistance, Math.min(maxStickDistance, offsetY))
+    setStickOffset({ x: clampedOffsetX, y: clampedOffsetY })
 
     const axes = [0, 0]
-    axes[0] = scaleJoystick(offsetX, stickLeft, stickRight, -1, 1)
-    axes[1] = scaleJoystick(offsetY, stickTop, stickBottom, -1, 1)
+    axes[0] = Math.max(-1, Math.min(1, offsetX / maxStickDistance))
+    axes[1] = Math.max(-1, Math.min(1, offsetY / maxStickDistance))
 
     if (Math.abs(axes[0] - oldAxis0) < 0.01 && Math.abs(axes[1] - oldAxis1) < 0.01) {
       return
@@ -102,49 +107,64 @@ export const TouchJoystick = () => {
     timePrev = t
 
     doSetCustomGamepad(null, axes)
-
-    const radians = Math.atan2(axes[1], axes[0])
-    const degrees = ((radians * (180 / Math.PI) + 450) % 360)
-    const stickScale = Math.min(Math.sqrt((axes[0] * axes[0] + axes[1] * axes[1]) / 2), 1)
-    const joystick = document.getElementById("touchjoystick-stick") as HTMLElement
-    joystick.style.transform = `rotate(${degrees}deg) scaleY(${stickScale})`
-    // If our stick scaling is too small it starts to look tiny and stupid.
-    // So just hide it in that case.
-    joystick.style.display = stickScale > 0.4 ? "block" : "none"
   }
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const handleStickPointerLeave = (event: React.PointerEvent) => {
-    const joystick = document.getElementById("touchjoystick-stick") as HTMLElement
-    joystick.style.display = "none"
-    doSetCustomGamepad(null, [0, 0])
-  }
-
-  const handleButtonsTouchStart = (event: React.TouchEvent) => {
-    const currentTarget = event.currentTarget as HTMLElement
-    let button0 = false
-    let button1 = false
-
-    for (let i=0; i<event.touches.length; i++) {
-      const touch = event.touches[i]
-      const unitY = (currentTarget.offsetTop - touch.clientY) / currentTarget.clientHeight
-      // Allow anything halfway or below to be button 0, otherwise button 1
-      if (unitY <= 0.5) {
-        button0 = true
-        toggleButton(0, true)
-      } else {
-        button1 = true
-        toggleButton(1, true)
-      }
+    if (stickDown[0] >= 0) {
+      setStickDown([-1, -1])
+      setStickOffset({ x: 0, y: 0 })
+      const joystick = document.getElementById("touchjoystick-stick") as HTMLElement
+      joystick.style.opacity = defaultOpacity
+      doSetCustomGamepad(null, [0, 0])
     }
-    doSetCustomGamepad([button0, button1], null)
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const handleButtonsTouchEnd = (event: React.TouchEvent) => {
-    toggleButton(0, false)
-    toggleButton(1, false)
-    doSetCustomGamepad([false, false], null)
+  const toggleButton = (buttonNumber: number, enabled: boolean) => {
+    const button = document.getElementById(`tj-button${buttonNumber}`) as HTMLElement
+    // Make the button look "pressed"
+    if (button) {
+      button.style.opacity = `${enabled ? 0.95 : defaultOpacity}`
+    }
+  }
+
+  const onButtonPointerDown = (event: React.PointerEvent<HTMLDivElement>, button: number) => {
+    event.currentTarget.setPointerCapture(event.pointerId)
+    const newButtonStates: [boolean, boolean] = [false, false]
+    if (button === 0) {
+      newButtonStates[0] = true
+      toggleButton(0, true)
+    } else {
+      newButtonStates[1] = true
+      toggleButton(1, true)
+    }
+    setButtonStates(newButtonStates)
+    doSetCustomGamepad(newButtonStates, null)
+  }
+
+  const onButtonPointerUp = (event: React.PointerEvent<HTMLDivElement>, button: number, reason: number) => {
+    if (reason === -1) {  // only for debugging
+      console.log(`button up: ${button} reason: ${reason}`)
+    }
+    if (event.currentTarget.hasPointerCapture(event.pointerId)) {
+      event.currentTarget.releasePointerCapture(event.pointerId)
+    }
+    // This might be called twice (once for PointerUp, again for PointerCancel or LostPointerCapture)
+    // Only fire property changes if the state actually changed.
+    let button0 = buttonStates[0]
+    let button1 = buttonStates[1]
+    if (button === 0 && button0) {
+      button0 = false
+      toggleButton(0, false)
+    }
+    if (button === 1 && button1) {
+      button1 = false
+      toggleButton(1, false)
+    }
+    if (button0 !== buttonStates[0] || button1 !== buttonStates[1]) {
+      setButtonStates([button0, button1])
+      doSetCustomGamepad([button0, button1], null)
+    }
   }
 
   if (!getUIStateBoolean("touchJoystick")) {
@@ -157,28 +177,58 @@ export const TouchJoystick = () => {
         draggable="false">
         <div
           className="tj-base-position"
-          onTouchMove={(event: React.TouchEvent) => event.preventDefault()}
-          onPointerEnter={handlePointerEnter}
-          onPointerMove={handlePointerMove}
-          onPointerLeave={handleStickPointerLeave}>
+          onContextMenu={(event) => event.preventDefault()}>
           <div
             id="touchjoystick-stick"
-          >
+            style={{
+              opacity: defaultOpacity,
+              "--touch-stick-offset-x": `${stickOffset.x}px`,
+              "--touch-stick-offset-y": `${stickOffset.y}px`,
+            } as React.CSSProperties}
+            onPointerDown={handlePointerDown}
+            onPointerMove={handlePointerMove}
+            onPointerUp={handleStickPointerLeave}
+            onPointerCancel={handleStickPointerLeave}
+            onLostPointerCapture={handleStickPointerLeave}
+            >
             <img
               className="tj-joystick-image"
-              src="/tj-joystick.png" />
+              src="/tj-joystick.png"
+              draggable={false} />
           </div>
         </div>
         <div
-          className="tj-buttons-base"
-          onTouchStart={handleButtonsTouchStart}
-          onTouchEnd={handleButtonsTouchEnd}>
-          <img id="tj-button0"
+          className="tj-buttons-base">
+          <div
+            id="tj-button0"
             className={"tj-buttons-both tj-buttons-button0"}
-            src="/tj-button.png" />
-          <img id="tj-button1"
+            style={{ opacity: defaultOpacity }}
+            onContextMenu={(event) => event.preventDefault()}
+            onPointerDown={(event) => onButtonPointerDown(event, 0)}
+            onPointerUp={(event) => onButtonPointerUp(event, 0, 0)}
+            onPointerCancel={(event) => onButtonPointerUp(event, 0, 1)}
+            onLostPointerCapture={(event) => onButtonPointerUp(event, 0, 2)}>
+            <img
+              className="tj-button-image"
+              src="/tj-button.png"
+              draggable={false}/>
+            <div className="tj-button-number">1</div>
+          </div>
+          <div
+            id="tj-button1"
             className={"tj-buttons-both tj-buttons-button1"}
-            src="/tj-button.png" />
+            style={{ opacity: defaultOpacity }}
+            onContextMenu={(event) => event.preventDefault()}
+            onPointerDown={(event) => onButtonPointerDown(event, 1)}
+            onPointerUp={(event) => onButtonPointerUp(event, 1, 0)}
+            onPointerCancel={(event) => onButtonPointerUp(event, 1, 1)}
+            onLostPointerCapture={(event) => onButtonPointerUp(event, 1, 2)}>
+            <img
+              className="tj-button-image"
+              src="/tj-button.png"
+              draggable={false}/>
+            <div className="tj-button-number">2</div>
+          </div>
         </div>
     </div>
   )
